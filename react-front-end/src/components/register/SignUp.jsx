d83ce47e5735fa03ee733ec99ec287d4270a1fa5
Alex <alex.andrul10@gmail.com>
2020-07-10 13:28:28 +0100

    change api end point singup

--- react-front-end/src/components/register/SignUp.jsx
+++ react-front-end/src/components/register/SignUp.jsx
@@ -36,7 +36,7 @@ class Registration extends Component {
       form: [{ title, firstName, lastName, email, jobInterest }],
     });
     event.preventDefault();
-    fetch(`https://sf-hackaton2020.herokuapp.com/api/posts`, {
+    fetch(`https://alex-sf-mongo.herokuapp.com/api/posts`, {
       method: "POST",
       body: JSON.stringify(this.state.form),
       headers: { "Content-Type": "application/json" },
