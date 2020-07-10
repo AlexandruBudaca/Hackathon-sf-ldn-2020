67b3c7c2b6d2880f0ab808ea326f8a011732bf7a
Babak <babakjahangiri@gmail.com>
2020-07-10 20:55:07 +0100

    init

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  //MongoDB  dummy data user info
-  // mongoURI: 'mongodb+srv://db-sf-ivi:seethefuture@cluster0-s9p6q.mongodb.net/test?retryWrites=true&w=majority'
-  mongoURI:
-    "mongodb+srv://minwwgo:codeyourfuture123@sf-hackathon.yop3r.mongodb.net/sf-hackathon?retryWrites=true&w=majority",
-};
