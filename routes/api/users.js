3443506ed74ac20d567b3f705a85ec75081c0397
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-07-13 16:01:22 +0100

    user login done , web token received.

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Load Input Validation
 const validateRegisterInput = require("../../validation/register");
@@ -11,19 +12,17 @@ const validateLoginInput = require("../../validation/login");
 const User = require("../../models/User");
 const Post = require("../../models/Post");
 
-// @route   Post api/users/signup
-// @desc    Tests post route
-// @access  Public
+// @route   POST api/users/signup
 
 router.post("/signup", (req, res) => {
   User.find({ email: req.body.email })
     .exec()
-    .then(user=>{
-      if(user.length >= 1){
+    .then((user) => {
+      if (user.length >= 1) {
         return res.status(409).json({
-          message:'Email exists'
-        })
-      }else{
+          message: "Email exists",
+        });
+      } else {
         bcrypt.hash(req.body.password, 10, (err, hash) => {
           if (err) {
             return res.status(500).json({ error: err });
@@ -50,11 +49,48 @@ router.post("/signup", (req, res) => {
               });
           }
         });
-
       }
+    });
+});
+
+// @route   POST api/users/login
+router.post("/login", (req, res) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
+        });
+      });
     })
-    
-  
+    .catch();
 });
 
 // @route   GET api/users
@@ -67,20 +103,19 @@ router.get("/", (req, res) => {
 });
 // @route   DELETE api/users
 
-router.delete('/:userId',(req,res)=>{
-  User.remove({_id:req.params.userId})
+router.delete("/:userId", (req, res) => {
+  User.remove({ _id: req.params.userId })
     .exec()
-    .then(result=>{
+    .then((result) => {
       res.status(200).json({
-        message: "User deleted"
-      })
+        message: "User deleted",
+      });
     })
-    .catch(err=>{
+    .catch((err) => {
       res.status(500).json({
-        error:err
-      })
-    })
-
-})
+        error: err,
+      });
+    });
+});
 
 module.exports = router;
