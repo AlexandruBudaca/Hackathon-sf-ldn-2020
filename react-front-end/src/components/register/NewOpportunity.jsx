32e9bd35e82591fd941fe72b95233a51e03a2534
Alex <alex.andrul10@gmail.com>
2020-07-16 16:00:53 +0100

    workink opp and post post opp

--- react-front-end/src/components/register/NewOpportunity.jsx
+++ react-front-end/src/components/register/NewOpportunity.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import imgSrc from "../../images/cyf_brand.png";
 import "./index.css";
 import { roles } from "./data/roles";
 import { types } from "./data/types";
 import { levels } from "./data/level";
 import { locations } from "./data/location";
 import Separator from "../Separator";
+import { Link } from "react-router-dom";
 /* 
 Registration is a shorter home to fill for users to sign up 
 quickly with the minimum info needed. Once a user is created they
@@ -17,10 +17,11 @@ const NewOpportunityForm = () => {
     company: "",
     role: "",
     type: "",
-    minimum: "",
+    minimum_proficiency: "",
     location: "",
     description: "",
   });
+  const [userCreated, setUserCreated] = useState(false);
 
   const handleChange = (event) => {
     const newOpportunity = {
@@ -30,30 +31,16 @@ const NewOpportunityForm = () => {
     setForm(newOpportunity);
   };
   const handleSubmit = (event) => {
-    //Email validation
-    // if (!form.email.includes("@")) {
-    //   return alert("Please enter Valid email");
-    // }
     //Creating JSON data for POST request to DB
 
     event.preventDefault();
-    fetch(`#`, {
+    fetch(`https://ancient-hamlet-95801.herokuapp.com/api/opp`, {
       method: "POST",
       mode: "cors",
       body: JSON.stringify(form),
       headers: { "Content-Type": "application/json" },
-    }).then((res) => {
-      //Clearing Form
-      // setForm({
-      //   company: "",
-      //   role: "",
-      //   type: "",
-      //   minimum: "",
-      //   location: "",
-      //   description: "",
-      // });
-    });
-    console.log(form);
+    }).then(setUserCreated(!userCreated));
+    event.target.reset();
   };
   // const handleJobInterest = (event) => {
   //   //checks if event is selected. If it is selected it is added to state, it unselected it is filtered out of array.
@@ -70,105 +57,115 @@ const NewOpportunityForm = () => {
   return (
     <section className="companiesSection">
       <Separator category={"New Opportunity"} />
-      <div>
-        <section className="mainRegister">
-          <div className="div-form-graduates">
-            <div className="div-form">
-              <form>
-                <ul>
-                  <li>
-                    <label>
-                      Company:
-                      <input
-                        className="select"
-                        name="company"
-                        onChange={handleChange}
-                        required
-                      ></input>
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      Role:
-                      <select
-                        className="select"
-                        name="role"
-                        onChange={handleChange}
-                        required
-                      >
-                        <option defaultValue="">Select your option</option>
-                        {roles.map((role) => (
-                          <option key={role.id}>{role.role}</option>
-                        ))}
-                      </select>
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      Type:
-                      <select
-                        className="select"
-                        name="type"
-                        onChange={handleChange}
-                        required
-                      >
-                        <option defaultValue="">Select your option</option>
-                        {types.map((type) => (
-                          <option key={type.id}>{type.type}</option>
-                        ))}
-                      </select>
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      Minimum Proficiency:
-                      <select
-                        className="select"
-                        name="minimum"
-                        onChange={handleChange}
-                        required
-                      >
-                        <option defaultValue="">Select your option</option>
-                        {levels.map((level) => (
-                          <option key={level.id}>{level.name}</option>
-                        ))}
-                      </select>
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      Location:
-                      <select
-                        className="select"
-                        name="location"
-                        onChange={handleChange}
-                        required
-                      >
-                        <option defaultValue="">Select your option</option>
-                        {locations.map((location) => (
-                          <option key={location.id}>{location.city}</option>
-                        ))}
-                      </select>
-                    </label>
-                  </li>
-                  <li>
-                    <label>
-                      Description:
-                      <textarea
-                        rows="4"
-                        cols="50"
-                        name="description"
-                        onChange={handleChange}
-                      ></textarea>
-                    </label>
-                  </li>
-                </ul>
-              </form>
-              <button onClick={handleSubmit}>Submit</button>
+      {userCreated ? (
+        <div className="singUpBack">
+          Your opportunity was created!
+          <Link to="/" className="backBtnSignUp">
+            Back
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <section className="mainRegister">
+            <div className="div-form-graduates">
+              <div className="div-form">
+                <form onSubmit={handleSubmit}>
+                  <ul>
+                    <li>
+                      <label>
+                        Company:
+                        <input
+                          className="select"
+                          name="company"
+                          onChange={handleChange}
+                          required
+                        ></input>
+                      </label>
+                    </li>
+                    <li>
+                      <label>
+                        Role:
+                        <select
+                          className="select"
+                          name="role"
+                          onChange={handleChange}
+                          required
+                        >
+                          <option defaultValue="">Select your option</option>
+                          {roles.map((role) => (
+                            <option key={role.id}>{role.role}</option>
+                          ))}
+                        </select>
+                      </label>
+                    </li>
+                    <li>
+                      <label>
+                        Type:
+                        <select
+                          className="select"
+                          name="type"
+                          onChange={handleChange}
+                          required
+                        >
+                          <option defaultValue="">Select your option</option>
+                          {types.map((type) => (
+                            <option key={type.id}>{type.type}</option>
+                          ))}
+                        </select>
+                      </label>
+                    </li>
+                    <li>
+                      <label>
+                        Minimum Proficiency:
+                        <select
+                          className="select"
+                          name="minimum_proficiency"
+                          onChange={handleChange}
+                          required
+                        >
+                          <option defaultValue="">Select your option</option>
+                          {levels.map((level) => (
+                            <option key={level.id}>{level.name}</option>
+                          ))}
+                        </select>
+                      </label>
+                    </li>
+                    <li>
+                      <label>
+                        Location:
+                        <select
+                          className="select"
+                          name="location"
+                          onChange={handleChange}
+                          required
+                        >
+                          <option defaultValue="">Select your option</option>
+                          {locations.map((location) => (
+                            <option key={location.id}>{location.city}</option>
+                          ))}
+                        </select>
+                      </label>
+                    </li>
+                    <li>
+                      <label>
+                        Description:
+                        <textarea
+                          rows="4"
+                          cols="50"
+                          name="description"
+                          onChange={handleChange}
+                        ></textarea>
+                      </label>
+                    </li>
+                  </ul>
+                  <button>Submit</button>
+                </form>
+              </div>
             </div>
-          </div>
-        </section>
-      </div>
+          </section>
+        </div>
+      )}
+      ;
     </section>
   );
 };
