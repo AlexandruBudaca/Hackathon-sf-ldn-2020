e3c1b96def4cfb4fc9592adbcc04c7701140271a
cockochan <56898295+cockochan@users.noreply.github.com>
2020-07-18 00:10:39 +0300

    grads evolved

--- react-front-end/src/components/sections/Graduates.jsx
+++ react-front-end/src/components/sections/Graduates.jsx
@@ -1,32 +1,11 @@
 import React, {  useState } from 'react';
-
 import './index.css'
 import Separator from '../Separator'
-const Graduates =()=> {
-  const [graduates, setGraduates]= useState ([
-        {
-          name: 'pasta',
-          imgSrc: 'https://via.placeholder.com/150',
-          description: 'hola', numberOfOffers: 4, publishedOn: '12 april 2019',
-          applyBy: '12 may 2019'
-        },
-        {
-          name: 'Bob', imgSrc: 'https://via.placeholder.com/150', description: 'Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', numberOfOffers: 4, publishedOn: '12 april 2019',
-          applyBy: '12 may 2019'
-        },
-        {
-          name: 'pasta', imgSrc: 'https://via.placeholder.com/150', description: 'Many desktop publishing packages and web page editors now useas their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', numberOfOffers: 4, publishedOn: '12 april 2019',
-          applyBy: '12 may 2019'
-        },
-        {
-          name: 'pasta', imgSrc: 'https://via.placeholder.com/150', description: 'Many desktop publishing packages and web page editors now useas their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', numberOfOffers: 4, publishedOn: '12 april 2019',
-          applyBy: '12 may 2019'
-        },
-        {
-          name: 'pasta', imgSrc: 'https://via.placeholder.com/150', description: 'Many desktop publishing packages and web page editors now useas their default model text, and a search for  will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', numberOfOffers: 4, publishedOn: '12 april 2019',
-          applyBy: '12 may 2019'
-        }])
 
+const Graduates =()=> {
+
+  const [graduates, setGraduates]= useState ([])
+  
         async function fetchGrads() {
           try {
             const res = await fetch(
