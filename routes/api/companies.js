eda7376b775d31af8f9591d42ba06971d989f939
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-07-21 13:54:59 +0100

    .env / routes get and login

--- routes/api/companies.js
+++ routes/api/companies.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Company = require("../../models/Company");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secretKey = require("../../config/keys").JWT_KEY;
 
-//testing GET api/companies
 
-router.get("/", (req, res)=>{
-  res.json({ message: "company is working" });
+//GET api/companies
+router.get("/", (req, res) => {
+  Company.find().then((compaines) => res.json(compaines));
 });
 
 //making POST api/companies/signup
@@ -52,6 +54,51 @@ router.post("/signup", (req, res)=> {
         });
       }
     });
+});
+
+// @route   POST api/companies/login
+router.post("/login", (req, res) => {
+
+  
+  Company.find({ companyEmail: req.body.companyEmail })
+    .exec()
+    .then((company) => {
+      if (company.length < 1) {
+        return res.status(401).json({
+          message: "Authorization failed",
+        });
+      }
+      bcrypt.compare(req.body.companyPassword, company[0].companyPassword, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Authorization failed",
+          });
+        }
+        if (result) {
+          let token = jwt.sign(
+            {
+              companyEmail: company[0].email,
+              compnayId: company[0]._id,
+            },
+            secretKey,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            companyName: company[0].companyName,
+
+            compnayEmail: company[0].companyEmail,
+            message: "Authorization successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Authorization failed",
+        });
+      });
+    })
+    .catch();
 });
 
 module.exports = router;
