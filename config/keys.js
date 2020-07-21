2b5e3cd1194f5e02a52e6c902e62b2165063f993
minwwgo <37987965+minwwgo@users.noreply.github.com>
2020-07-21 14:25:07 +0100

    congfig change

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,16 @@
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
-dotenv.config();
+// dotenv.config();
 
+// module.exports = {
+//   //MongoDB  dummy data user info
+//   mongoURI: process.env.MONGOLAB_URI,
+//   JWT_KEY: process.env.JWT_KEY
+// };
 module.exports = {
   //MongoDB  dummy data user info
-  mongoURI: process.env.MONGOLAB_URI,
-  JWT_KEY: process.env.JWT_KEY
-};
+  mongoURI:
+    "mongodb+srv://minwwgo:codeyourfuture123@sf-hackathon.yop3r.mongodb.net/sf-hackathon?retryWrites=true&w=majority",
+    JWT_KEY:"secret"
+  
+}; 
