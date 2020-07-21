60c1fd8a65e62a0cab9c6d9352611f828f269670
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2020-07-21 22:25:52 +0100

    companies

--- react-front-end/src/components/sections/Graduates.jsx
+++ react-front-end/src/components/sections/Graduates.jsx
@@ -1,58 +1,52 @@
-import React, {  useState, useEffect } from 'react';
-import './index.css'
-import Separator from '../Separator'
-import Gr from '../../MockData/Graduates.json'
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import Separator from "../Separator";
+import Gr from "../../MockData/Graduates.json";
 
-const Graduates =()=> {
-  const [graduates, setGraduates]= useState(Gr)
+const Graduates = () => {
+  const [graduates, setGraduates] = useState(Gr);
   useEffect(() => {
     async function fetchGrads() {
       try {
         const res = await fetch(
           "https://ancient-hamlet-95801.herokuapp.com/api/users"
         );
-        const json = await res.json()
-        setGraduates(json)
+        const json = await res.json();
+        setGraduates(json);
       } catch (error) {
         console.error(error.message);
       }
     }
     fetchGrads();
   }, []);
-    return (
-      <section className="companiesSection">
-      <Separator category="Graduates"/>
+  return (
+    <section className="companiesSection">
+      <Separator category="Graduates" />
       {graduates.map((grad) => {
-      return (
-          <article className="opp_article row" key={grad._id}>
-      
-              <img className='companyLogo col-1'
+        return (
+          <article className="opp_article row alignedTop" key={grad._id}>
+            <div className="justified">
+              <img
+                className="companyLogo"
                 src="https://github.com/AlexandruBudaca/Hackaton-sf-ldn-2020/blob/babak/Design/images%20files/opp-icon/role-frontend.gif?raw=true"
                 alt=""
               />
-     
-              <div className='oppColumn col-3'>
-                {/* <Link to="/Opportunity">
-                </Link> */}
-                <p>{grad.firstName}</p>
-                <p>{grad.lastName}</p>
-              
-                <span>{grad.lastName}</span>
-               
-               <span>{grad.role}</span>
-                </div>
-                <div className='oppColumn col-3'>
-                <span>{grad.description}</span>
             </div>
-              
 
-           
+            {/* <Link to="/Opportunity">
+                </Link> */}
+            <p className="centered justified col-3">{grad.firstName}</p>
+
+            <span className="centered justified col-3">{grad.lastName}</span>
+
+            <span className="centered justified col-3">{grad.role}</span>
+
+            <span className="centered justified col-3">{grad.description}</span>
           </article>
         );
       })}
     </section>
+  );
+};
 
-    )}
-
-
-export default Graduates
+export default Graduates;
