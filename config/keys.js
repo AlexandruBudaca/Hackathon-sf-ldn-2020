dfc26120d1ce716b0fae0ec66e997a95c71e3b6e
Alex <alex.andrul10@gmail.com>
2020-10-06 17:25:11 +0100

    change mongodb

--- config/keys.js
+++ config/keys.js
@@ -2,11 +2,6 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-// module.exports = {
-//   //MongoDB  dummy data user info
-//   mongoURI: process.env.MONGOLAB_URI,
-//   JWT_KEY: process.env.JWT_KEY
-// };
 module.exports = {
   //MongoDB  dummy data user info
   mongoURI: `mongodb+srv://AlexandruBudaca:${process.env.password}@alex.njtpl.mongodb.net/${process.env.mongoCollection}?retryWrites=true&w=majority`,
