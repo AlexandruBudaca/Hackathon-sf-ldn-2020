405dfc30a261d65338be7019a03f2071e703aae7
Alex <alex.andrul10@gmail.com>
2020-10-06 17:24:26 +0100

    change mongodb

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 
-// dotenv.config();
+dotenv.config();
 
 // module.exports = {
 //   //MongoDB  dummy data user info
@@ -9,8 +9,6 @@
 // };
 module.exports = {
   //MongoDB  dummy data user info
-  mongoURI:
-    "mongodb+srv://minwwgo:codeyourfuture123@sf-hackathon.yop3r.mongodb.net/sf-hackathon?retryWrites=true&w=majority",
-    JWT_KEY:"secret"
-  
-}; 
+  mongoURI: `mongodb+srv://AlexandruBudaca:${process.env.password}@alex.njtpl.mongodb.net/${process.env.mongoCollection}?retryWrites=true&w=majority`,
+  JWT_KEY: "secret",
+};
